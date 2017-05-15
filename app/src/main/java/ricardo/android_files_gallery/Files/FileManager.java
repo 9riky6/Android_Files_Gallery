07a8d7c6c8182763cba3194908d9cf27b77c0531
Nil Garcia <nilarrus69@gmail.com>
2017-05-15 20:17:05 +0200

    imagenes y tamaños

--- app/src/main/java/ricardo/android_files_gallery/Files/FileManager.java
+++ app/src/main/java/ricardo/android_files_gallery/Files/FileManager.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,13 +15,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 import ricardo.android_files_gallery.MainActivity;
 import ricardo.android_files_gallery.R;
 
 
-public class FileManager extends AppCompatActivity{
+public class FileManager extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,26 +30,26 @@ public class FileManager extends AppCompatActivity{
         setContentView(R.layout.file_manager);
 
         Intent intent = getIntent();
-        final TextView path = (TextView) findViewById(R.id.path);
+
 
         //Aixo fa que sempre deixi la ultima barra
         String pathtemp = intent.getStringExtra("path");
-        pathtemp = pathtemp.substring(0,pathtemp.lastIndexOf("/")+1);
+        pathtemp = pathtemp.substring(0, pathtemp.lastIndexOf("/") + 1);
 
-        path.setText(pathtemp);
-        File file = new File(path.getText().toString());
+
+        File file = new File(pathtemp);
         final TableLayout tabla = (TableLayout) findViewById(R.id.Contenido);
         tabla.removeAllViews();
 
         boolean home = true;
 
         //Afegeix el directori .. pare
-        if (!(file.getAbsolutePath().equals(pathtemp))){
+        if (!(file.getAbsolutePath().equals(pathtemp))) {
             //Fem layout amb taula + inflater
             TableRow row = (TableRow) findViewById(R.id.Item);
-            LayoutInflater inflater =(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             //pasem el layoud la row de la taula.
-            TableRow rowLayout = (TableRow) inflater.inflate(R.layout.item_list,row, false);
+            TableRow rowLayout = (TableRow) inflater.inflate(R.layout.item_list, row, false);
 
             //Fem la imatge.
             ImageView imatge = (ImageView) rowLayout.findViewById(R.id.icono);
@@ -73,50 +75,48 @@ public class FileManager extends AppCompatActivity{
         final File[] children = file.listFiles();
         Arrays.sort(children);
 
-        for(int i=0;i<children.length;i++){
+        for (int i = 0; i < children.length; i++) {
 
             //Fem layout amb taula + inflater
             TableRow row = (TableRow) findViewById(R.id.Item);
-            LayoutInflater inflater =
-                    (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            TableRow rowLayout = (TableRow) inflater.inflate(R.layout.item_list,row, false);
+            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            TableRow rowLayout = (TableRow) inflater.inflate(R.layout.item_list, row, false);
 
             //Fem la imatge.
             ImageView imatge = (ImageView) rowLayout.findViewById(R.id.icono);
-            if(children[i].isDirectory()) {
+            if (children[i].isDirectory()) {
                 imatge.setImageResource(R.drawable.folder);
-            }else {
+            } else {
                 imatge.setImageResource(R.drawable.unknown);
             }
 
             //Fem el nom.
             TextView cami = (TextView) rowLayout.findViewById(R.id.textViewChildren);
             cami.setText(children[i].getName());
-            cami.setTextSize(16);
+            // cami.setTextSize(16);
 
             //Si es un fitxer, que indiqui la mida i el tipus de fitxer
-            //Si es un directori, que indiqui que ho es
+            //Si es un directori, que indiqui que ho es i el pesa.
             TextView extension = (TextView) rowLayout.findViewById(R.id.textViewType);
             TextView size = (TextView) rowLayout.findViewById(R.id.textViewSize);
-            if (children[i].isFile()){
+            if (children[i].isFile()) {//arxiu
                 //tamaño
                 long temp = children[i].length();
                 //PER MILLORAR
-                size.setText(TamanyFitxer(temp));
+                size.setText(getSizefile(temp));
 
                 //tipo
                 String temp2 = children[i].getName();
-
-                if(temp2.contains("."))
-                    extension.setText("Fitxer " + temp2.substring(temp2.lastIndexOf(".")+1));
-
-                else{
+                Log.d("nombre ",temp2);
+                if (temp2.contains(".")) {
+                    extension.setText("Fitxer " + temp2.substring(temp2.lastIndexOf(".") + 1));
+                    getImatge(temp2, imatge);
+                } else {
                     extension.setText("Fitxer");
                 }
-            }
-            else{
+            } else { //directori / capeta
                 //PER MILLORAR
-                size.setText(TamanyTotalfitxers(children[i].getAbsolutePath().toString()));
+                size.setText(getSize(children[i].getAbsolutePath()));
                 extension.setText("Directori");
             }
 
@@ -124,10 +124,10 @@ public class FileManager extends AppCompatActivity{
             if (home)
                 tabla.addView(rowLayout);
             else
-                tabla.addView(rowLayout, i+1);
+                tabla.addView(rowLayout, i + 1);
         }
         //Listener per navegar
-        for(int i=0;i<tabla.getChildCount();i++){
+        for (int i = 0; i < tabla.getChildCount(); i++) {
             final boolean finalRoot = home;
             final int finalI = i;
             final int aux;
@@ -138,91 +138,228 @@ public class FileManager extends AppCompatActivity{
             else
                 aux = 0;
 
+            final String finalPathtemp = pathtemp;
             tabla.getChildAt(i).setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     //Exclusiu al ..
-                    if(!finalRoot && finalI == 0){
-                        String cami = path.getText().toString();
+                    if (!finalRoot && finalI == 0) {
+                        String cami = finalPathtemp;
 
                         //Si acaba amb una barra, la treu
-                        if (cami.charAt(cami.length()-1) == '/')
-                            cami = cami.substring(0,cami.length()-1);
+                        if (cami.charAt(cami.length() - 1) == '/')
+                            cami = cami.substring(0, cami.length() - 1);
 
                         //Agafa tot el path fins la ultima barra, inclosa
-                        cami = cami.substring(0,cami.lastIndexOf("/")+1);
+                        cami = cami.substring(0, cami.lastIndexOf("/") + 1);
 
                         Intent intent2 = new Intent(getApplicationContext(), FileManager.class);
-                        intent2.putExtra("path",cami);
+                        intent2.putExtra("path", cami);
                         startActivity(intent2);
                     }
                     //Suposant que sigui un directori
-                    else if(children[finalI+aux].isDirectory()){
+                    else if (children[finalI + aux].isDirectory()) {
                         Intent intent2 = new Intent(getApplicationContext(), FileManager.class);
-                        intent2.putExtra("path",children[finalI+aux].getAbsolutePath()+"/");
+                        intent2.putExtra("path", children[finalI + aux].getAbsolutePath() + "/");
                         startActivity(intent2);
-                    }
-                    else{
+                    } else {
                         //
                         Intent intent2 = new Intent(getApplicationContext(), MainActivity.class);
-                        intent2.putExtra("path",children[finalI+aux].getAbsolutePath());
+                        intent2.putExtra("path", children[finalI + aux].getAbsolutePath());
                         startActivity(intent2);
                     }
                 }
             });
         }
-    for(int i=0; i<tabla.getChildCount();i++){
-        final int finalI = i;
-        tabla.getChildAt(i).setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                Toast.makeText(FileManager.this,children[finalI-1].getName(), Toast.LENGTH_LONG).show();
-               // Toast.makeText(FileManager.this,children[finalI-1].getAbsolutePath(), Toast.LENGTH_LONG).show();//falta la / al final
-                return true;
-            }
-        });
-    }
+        for (int i = 0; i < tabla.getChildCount(); i++) {
+            final int finalI = i;
+            tabla.getChildAt(i).setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Toast.makeText(FileManager.this, children[finalI - 1].getName(), Toast.LENGTH_LONG).show();
+                    // Toast.makeText(FileManager.this,children[finalI-1].getAbsolutePath(), Toast.LENGTH_LONG).show();//falta la / al final
+                    return true;
+                }
+            });
+        }
 
     }
-    //PER MILLORAR
+    private void getImatge(String name, ImageView imatge) {
+        String extencionFile = name.substring(name.lastIndexOf(".") + 1);
+        Log.d("Contenido extencionFile", extencionFile);
 
-    public String TamanyTotalfitxers(String ruta){
-        File arxiu = new File (ruta);
+        if (extencionFile.equalsIgnoreCase("jpg")) {
+            imatge.setImageResource(R.drawable.jpg);
+
+        } else if (extencionFile.equalsIgnoreCase(("ai"))) {
+            imatge.setImageResource(R.drawable.ai);
+
+        } else if (extencionFile.equalsIgnoreCase(("apk"))) {
+            imatge.setImageResource(R.drawable.apk);
+
+        } else if (extencionFile.equalsIgnoreCase(("avi"))) {
+            imatge.setImageResource(R.drawable.avi);
+
+        } else if (extencionFile.equalsIgnoreCase(("bmp"))) {
+            imatge.setImageResource(R.drawable.bmp);
+
+        } else if (extencionFile.equalsIgnoreCase(("cad"))) {
+            imatge.setImageResource(R.drawable.cad);
+
+        } else if (extencionFile.equalsIgnoreCase(("cdr"))) {
+            imatge.setImageResource(R.drawable.cdr);
+
+        } else if (extencionFile.equalsIgnoreCase(("css"))) {
+            imatge.setImageResource(R.drawable.css);
+
+        } else if (extencionFile.equalsIgnoreCase(("dat"))) {
+            imatge.setImageResource(R.drawable.dat);
+
+        } else if (extencionFile.equalsIgnoreCase(("dll"))) {
+            imatge.setImageResource(R.drawable.dll);
+
+        } else if (extencionFile.equalsIgnoreCase(("dmg"))) {
+            imatge.setImageResource(R.drawable.dmg);
+
+        } else if (extencionFile.equalsIgnoreCase(("doc"))) {
+            imatge.setImageResource(R.drawable.doc);
+
+        } else if (extencionFile.equalsIgnoreCase(("eps"))) {
+            imatge.setImageResource(R.drawable.eps);
+
+        } else if (extencionFile.equalsIgnoreCase(("fla"))) {
+            imatge.setImageResource(R.drawable.fla);
+
+        } else if (extencionFile.equalsIgnoreCase(("flv"))) {
+            imatge.setImageResource(R.drawable.flv);
+
+        } else if (extencionFile.equalsIgnoreCase(("gif"))) {
+            imatge.setImageResource(R.drawable.gif);
+
+        } else if (extencionFile.equalsIgnoreCase(("html"))) {
+            imatge.setImageResource(R.drawable.html);
+
+        } else if (extencionFile.equalsIgnoreCase(("indd"))) {
+            imatge.setImageResource(R.drawable.indd);
+
+        } else if (extencionFile.equalsIgnoreCase(("iso"))) {
+            imatge.setImageResource(R.drawable.iso);
+
+        } else if (extencionFile.equalsIgnoreCase(("js"))) {
+            imatge.setImageResource(R.drawable.js);
+
+        } else if (extencionFile.equalsIgnoreCase(("midi"))) {
+            imatge.setImageResource(R.drawable.midi);
+
+        } else if (extencionFile.equalsIgnoreCase(("mkv"))) {
+            imatge.setImageResource(R.drawable.mkv);
+
+        } else if (extencionFile.equalsIgnoreCase(("mov"))) {
+            imatge.setImageResource(R.drawable.mov);
+
+        } else if (extencionFile.equalsIgnoreCase(("mp3"))) {
+            imatge.setImageResource(R.drawable.mp3);
+
+        } else if (extencionFile.equalsIgnoreCase(("mpg"))) {
+            imatge.setImageResource(R.drawable.mpg);
+
+        } else if (extencionFile.equalsIgnoreCase(("php"))) {
+            imatge.setImageResource(R.drawable.php);
+
+        } else if (extencionFile.equalsIgnoreCase(("pdf"))) {
+            imatge.setImageResource(R.drawable.pdf);
+
+        } else if (extencionFile.equalsIgnoreCase(("png"))) {
+            imatge.setImageResource(R.drawable.png);
+
+        } else if (extencionFile.equalsIgnoreCase(("ppt"))) {
+            imatge.setImageResource(R.drawable.ppt);
+
+        } else if (extencionFile.equalsIgnoreCase(("ps"))) {
+            imatge.setImageResource(R.drawable.ps);
+
+        } else if (extencionFile.equalsIgnoreCase(("psd"))) {
+            imatge.setImageResource(R.drawable.psd);
+
+        } else if (extencionFile.equalsIgnoreCase(("rar"))) {
+            imatge.setImageResource(R.drawable.rar);
+
+        } else if (extencionFile.equalsIgnoreCase(("raw"))) {
+            imatge.setImageResource(R.drawable.raw);
+
+        } else if (extencionFile.equalsIgnoreCase(("sql"))) {
+            imatge.setImageResource(R.drawable.sql);
+
+        } else if (extencionFile.equalsIgnoreCase(("svg"))) {
+            imatge.setImageResource(R.drawable.svg);
+
+        } else if (extencionFile.equalsIgnoreCase(("tif"))) {
+            imatge.setImageResource(R.drawable.tif);
+
+        } else if (extencionFile.equalsIgnoreCase(("txt"))) {
+            imatge.setImageResource(R.drawable.txt);
+
+        } else if (extencionFile.equalsIgnoreCase(("wmv"))) {
+            imatge.setImageResource(R.drawable.wmv);
+
+        } else if (extencionFile.equalsIgnoreCase(("xls"))) {
+            imatge.setImageResource(R.drawable.xls);
+
+        } else if (extencionFile.equalsIgnoreCase(("xml"))) {
+            imatge.setImageResource(R.drawable.xml);
+
+        } else if (extencionFile.equalsIgnoreCase(("zip"))) {
+            imatge.setImageResource(R.drawable.zip);
+
+        } else if (extencionFile.equalsIgnoreCase(("aac"))) {
+            imatge.setImageResource(R.drawable.aac);
+
+        } else if (extencionFile.equalsIgnoreCase(("docx"))) {
+            //TODO: <3 FALATA INCONO
+            //imatge.setImageResource(R.drawable.docx);
+        } else {
+           imatge.setImageResource(R.drawable.unknown);
+        }
+    }
+    //PER MILLORAR mirar lo de les carpetes.
+    public String getSize(String ruta) {//directori/carpeta
+        DecimalFormat df = new DecimalFormat("####");
+        File carpeta = new File(ruta);
         //ESPAI TOTAL
         String numero;//bytes
-        long auxNum = arxiu.getTotalSpace()-arxiu.getFreeSpace();//numero temporal per pasar el valors i fer la combercio
-        long num2 = arxiu.getTotalSpace()-arxiu.getFreeSpace() ;
-        if(num2%1024 != 0){ //kilobyte
-            numero = String.valueOf(auxNum)+" B";
+        float auxNum = (float) carpeta.getTotalSpace() - carpeta.getFreeSpace();//numero temporal per pasar el valors i fer la combercio
+        float num2 = (float) carpeta.getTotalSpace() - carpeta.getFreeSpace();
+        if (num2 % 1024 != 0) {
 
-        }else if(num2%1024 == 0 && num2/1048576 ==0){
+            numero = String.valueOf(df.format(Math.ceil(auxNum / 1024))) + " KB Carpeta";
 
-            numero = String.valueOf(auxNum/1024)+" KB";
+        } else if (num2 / 1048576 != 0) {
 
-        }else if(num2/1048576 !=0 && num2/1073741824 ==0){
+            numero = String.valueOf(df.format(Math.ceil(auxNum / 1048576))) + " MB Carpeta";
 
-            numero = String.valueOf(auxNum/1048576)+" MB";
+        } else {
 
-        }else{
-
-            numero = String.valueOf(auxNum/1073741824)+" GB";
+            numero = String.valueOf(df.format(Math.ceil(auxNum / 1073741824))) + " GB Carpeta";
         }
         return numero;
     }
-    public String  TamanyFitxer(long num){
-        int n = (int) num;
-        String valor= null;
-        if (num %1024 != 0 && num/1024 !=0) {
-            valor = String.valueOf(num)+" B";
-        }else /*if(n%1024 != 0 && n/1048576==0)*/{
 
-            valor = String.valueOf(num/1024)+" KB";
+    public String getSizefile(long num) {//arxius
+        DecimalFormat df = new DecimalFormat("###0.#");
+        float n = (float) num;
+        String valor;
+        if (num % 1024 != 0 && num / 1024 == 0) {
+            valor = String.valueOf(n) + " B";
+        } else if (num / 1024 != 0) {
 
-        }/*else if(n%1048576 != 0 && n/1073741824==0) {
+            valor = df.format(n / 1024) + " KB";
 
-            valor = String.valueOf(num / 1048576) + " MB";
-        }else{
-            valor = String.valueOf(num/1073741824)+" GB";
-        }*/
+        } else if (num % 1048576 != 0) {
+
+            valor = df.format(n / 1048576) + " MB";
+        } else {
+
+            valor = df.format(n / 1073741824) + " GB";
+        }
         return valor;
     }
 }
